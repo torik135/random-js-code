ae3794f269839c22c547ab33f212ca43677ebb32
torik135 <riqibrekele@gmail.com>
2022-09-13 05:30:30 +0700

    add: move 0 to the end

---
+++ move0.js
@@ -0,0 +1,31 @@
+/**
+ * Move all the 0's to the end
+ * Ex:
+ * INPUT:  [0, 1, 0, 3, 12]
+ * OUTPUT: [1, 3, 12, 0, 0]
+ * */
+
+function moveZero(arr) {
+  // point to the last element
+  let lastEl = arr.length - 1;
+
+  // looping the arr
+  for (let i = 0; i <= lastEl; i++) {
+    // if the element is zero
+    if (arr[i] == 0) {
+      // swap the element position to the end
+      [arr[i], arr[lastEl]] = [arr[lastEl], arr[i]];
+
+      // decrement the pointer / move left
+      lastEl--;
+    }
+  }
+
+  return arr;
+}
+
+const numEl1 = [0, 1, 0, 3, 12];
+
+const moveToEnd = moveZero(numEl1);
+
+console.log(moveToEnd);
