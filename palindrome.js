1afabb2b3732ca9d4d9648bc694fa65744c0e4d0
torik135 <riqibrekele@gmail.com>
2022-09-16 18:54:15 +0700

    case sensitive & not case sensitive

--- palindrome.js
+++ palindrome.js
@@ -1,12 +1,37 @@
-function palindrome(arr){
+// case sensitive
+function palindromeCaseSensitive(arr) {
   let arrLen = arr.length;
-  if (arrLen < 2) return 'Palindrome';
+  if (arrLen < 2) return "Palindrome";
 
-  if (arr[0] !== arr[arrLen - 1]){
-    return 'Not Palindrome';
+  if (arr[0] !== arr[arrLen - 1]) {
+    return "Not Palindrome";
   }
 
-  return palindrome(arr.slice(1, arr.length - 1));
-};
+  return palindromeCaseSensitive(arr.slice(1, arr.length - 1));
+}
 
-console.log(palindrome('radar'));
+console.log('Case Sensitive');
+console.log("r: ", palindromeCaseSensitive("r"));
+console.log("ra: ", palindromeCaseSensitive("ra"));
+console.log("radar: ", palindromeCaseSensitive("radar"));
+console.log("Radar: ", palindromeCaseSensitive("Radar"));
+
+// not case sensitive
+function palindromeCaseNotSensitive(arr) {
+  let arrLen = arr.length;
+  if (arrLen < 2) return "Palindrome";
+
+  let arrLower = arr.toLowerCase();
+
+  if (arrLower[0] !== arrLower[arrLen - 1]) {
+    return "Not Palindrome";
+  }
+
+  return palindromeCaseNotSensitive(arr.slice(1, arr.length - 1));
+}
+
+console.log(`\nNot Case Sensitive`);
+console.log("r: ", palindromeCaseNotSensitive("r"));
+console.log("ra: ", palindromeCaseNotSensitive("ra"));
+console.log("radar: ", palindromeCaseNotSensitive("radar"));
+console.log("Radar: ", palindromeCaseNotSensitive("Radar"));
--- palindromeReverse.js
+++ palindromeReverse.js
@@ -1,4 +1,4 @@
-function palindrome(arr) {
+function palindromeCaseSensitive(arr) {
   if (arr.length < 2) return "to short!";
 
   let arrInp = arr.trim().replace(" ", "");
@@ -8,11 +8,36 @@ function palindrome(arr) {
   return "is Not Palindrome";
 }
 
-console.log("radar: ", palindrome("radar"));
-console.log("bed: ", palindrome("bed"));
-console.log("wing: ", palindrome("wing"));
-console.log("hair: ", palindrome("hair"));
-console.log("bob: ", palindrome("bob"));
-console.log("bob: ", palindrome("bob"));
-console.log("madam: ", palindrome("madam"));
-console.log("nurses run: ", palindrome("nurses run"));
+function palindromeNotCaseSensitive(arr) {
+  if (arr.length < 2) return "to short!";
+
+  let arrInp = arr.trim().replace(" ", "").toLowerCase();
+  let arrReverse = arr.split("").reverse().join("").trim().replace(" ", "").toLowerCase();
+
+  if (arrInp === arrReverse) return "is Palindrome";
+  return "is Not Palindrome";
+}
+
+console.log(`\nCase Sensitive:`);
+console.log("r: ", palindromeCaseSensitive("r"));
+console.log("Radar: ", palindromeCaseSensitive("Radar"));
+console.log("radar: ", palindromeCaseSensitive("radar"));
+console.log("bed: ", palindromeCaseSensitive("bed"));
+console.log("wing: ", palindromeCaseSensitive("wing"));
+console.log("hair: ", palindromeCaseSensitive("hair"));
+console.log("bob: ", palindromeCaseSensitive("bob"));
+console.log("bob: ", palindromeCaseSensitive("bob"));
+console.log("madam: ", palindromeCaseSensitive("madam"));
+console.log("nurses run: ", palindromeCaseSensitive("nurses run"));
+
+console.log(`\nNot Case Sensitive:`);
+console.log("r: ", palindromeNotCaseSensitive("r"));
+console.log("Radar: ", palindromeNotCaseSensitive("Radar"));
+console.log("radar: ", palindromeNotCaseSensitive("radar"));
+console.log("bed: ", palindromeNotCaseSensitive("bed"));
+console.log("wing: ", palindromeNotCaseSensitive("wing"));
+console.log("hair: ", palindromeNotCaseSensitive("hair"));
+console.log("bob: ", palindromeNotCaseSensitive("bob"));
+console.log("bob: ", palindromeNotCaseSensitive("bob"));
+console.log("madam: ", palindromeNotCaseSensitive("madam"));
+console.log("nurses run: ", palindromeNotCaseSensitive("nurses run"));
