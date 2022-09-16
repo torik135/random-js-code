60a626ef4bf98a443e76c5afbb35ac97badbb09e
torik135 <riqibrekele@gmail.com>
2022-09-16 13:53:28 +0700

    fix: string formatting

--- palindromeReverse.js
+++ palindromeReverse.js
@@ -1,10 +1,10 @@
 function palindrome(arr) {
   if (arr.length < 2) return "to short!";
-  
-  let arrInp = arr.split("");
-  let arrReverse = arr.split("").reverse();
-  
-  if (arrInp[0] === arrReverse[0]) return "is Palindrome";
+
+  let arrInp = arr.trim().replace(" ", "");
+  let arrReverse = arr.split("").reverse().join("").trim().replace(" ", "");
+
+  if (arrInp === arrReverse) return "is Palindrome";
   return "is Not Palindrome";
 }
 
@@ -12,3 +12,7 @@ console.log("radar: ", palindrome("radar"));
 console.log("bed: ", palindrome("bed"));
 console.log("wing: ", palindrome("wing"));
 console.log("hair: ", palindrome("hair"));
+console.log("bob: ", palindrome("bob"));
+console.log("bob: ", palindrome("bob"));
+console.log("madam: ", palindrome("madam"));
+console.log("nurses run: ", palindrome("nurses run"));
