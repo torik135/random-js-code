6627002c07b36b9bd32a3d8d0ac3a5000d2744b0
torik135 <riqibrekele@gmail.com>
2022-09-16 19:20:22 +0700

    fix: case sensitive & not, remove space & blank space

--- palindrome.js
+++ palindrome.js
@@ -1,37 +1,50 @@
 // case sensitive
 function palindromeCaseSensitive(arr) {
-  let arrLen = arr.length;
+  let arrIn = arr.replace(" ", "").trim();
+  let arrLen = arrIn.length;
   if (arrLen < 2) return "Palindrome";
 
-  if (arr[0] !== arr[arrLen - 1]) {
+  if (arrIn[0] !== arrIn[arrLen - 1]) {
     return "Not Palindrome";
   }
 
-  return palindromeCaseSensitive(arr.slice(1, arr.length - 1));
+  return palindromeCaseSensitive(arrIn.slice(1, arrIn.length - 1));
 }
 
-console.log('Case Sensitive');
+console.log(`\nCase Sensitive:`);
 console.log("r: ", palindromeCaseSensitive("r"));
-console.log("ra: ", palindromeCaseSensitive("ra"));
-console.log("radar: ", palindromeCaseSensitive("radar"));
 console.log("Radar: ", palindromeCaseSensitive("Radar"));
+console.log("radar: ", palindromeCaseSensitive("radar"));
+console.log("bed: ", palindromeCaseSensitive("bed"));
+console.log("wing: ", palindromeCaseSensitive("wing"));
+console.log("hair: ", palindromeCaseSensitive("hair"));
+console.log("bob: ", palindromeCaseSensitive("bob"));
+console.log("madam: ", palindromeCaseSensitive("madam"));
+console.log("nurses run: ", palindromeCaseSensitive("nurses run"));
 
 // not case sensitive
-function palindromeCaseNotSensitive(arr) {
-  let arrLen = arr.length;
+function palindromeNotCaseSensitive(arr) {
+  let arrIn = arr.replace(" ", "").trim();
+  let arrLen = arrIn.length;
   if (arrLen < 2) return "Palindrome";
 
-  let arrLower = arr.toLowerCase();
+  let arrLower = arrIn.toLowerCase();
 
   if (arrLower[0] !== arrLower[arrLen - 1]) {
     return "Not Palindrome";
   }
 
-  return palindromeCaseNotSensitive(arr.slice(1, arr.length - 1));
+  return palindromeNotCaseSensitive(arrIn.slice(1, arrIn.length - 1));
 }
 
-console.log(`\nNot Case Sensitive`);
-console.log("r: ", palindromeCaseNotSensitive("r"));
-console.log("ra: ", palindromeCaseNotSensitive("ra"));
-console.log("radar: ", palindromeCaseNotSensitive("radar"));
-console.log("Radar: ", palindromeCaseNotSensitive("Radar"));
+console.log(`\nNot Case Sensitive:`);
+console.log("r: ", palindromeNotCaseSensitive("r"));
+console.log("Radar: ", palindromeNotCaseSensitive("Radar"));
+console.log("radar: ", palindromeNotCaseSensitive("radar"));
+console.log("bed: ", palindromeNotCaseSensitive("bed"));
+console.log("wing: ", palindromeNotCaseSensitive("wing"));
+console.log("hair: ", palindromeNotCaseSensitive("hair"));
+console.log("bob: ", palindromeNotCaseSensitive("bob"));
+console.log("madam: ", palindromeNotCaseSensitive("madam"));
+// nurses run suppose to return a Palindrome
+console.log("nurses run: ", palindromeNotCaseSensitive("nurses run"));
