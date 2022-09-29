050efa76fcd6885811d548b64861051a3f51a7d1
torik135 <riqibrekele@gmail.com>
2022-09-29 17:19:39 +0700

    remove unneeded lines

--- RegexArrItem.js
+++ RegexArrItem.js
@@ -1,30 +1,25 @@
 /**
  * checking and filter out the char inside string in the array.
- * 
+ *
  * ex:
  * IN: ['t$$h$is', 'i$s', 'g$$r$$ea$t']
  * OUT: 'THIS IS GREAT'
  * */
 
 function checkRegex(arr) {
-  let arrIn = arr;
-  // char to filter out
-  const charOut = /[$]+/;
   // empty array
   let empArr = [];
   // iter arr
-  for(let i  = 0; i < arrIn.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     // aplit arr item and join the split item
-    let arr2 = arrIn[i].split(charOut).join("");
+    let arr2 = arr[i].split(/[$]+/).join("");
     // push arr2 into empty array
     empArr.push(arr2);
   }
-
   // turn array into string with join and uppercase the string
-  let result = empArr.join(" ").toUpperCase();
-  return result;
-};
+  return empArr.join(" ").toUpperCase();
+}
 
-const arr1 = ['t$$h$is', 'i$s', 'g$$r$$ea$t'];
+const arr1 = ["t$$h$is", "i$s", "g$$r$$ea$t"];
 
 console.log(checkRegex(arr1));
